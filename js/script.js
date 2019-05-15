3f8140b62da7e43eba2b3f9954a8e77f7b32b0c1
Ruben <ruben.pinto.91@gmail.com>
2019-05-15 22:34:14 +0100

    fixed the horizontal scrolling bug on mobile in the top nav

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
-
         //burger animation
         burger.classList.toggle('toggle');
     });

474bae553ae40d1d83c5290a47cda3f501916034
Ruben <ruben.pinto.91@gmail.com>
2019-05-16 22:08:36 +0100

    created a new navbar that is sticky to the top of the page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector ('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector ('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', ()=> {
-        //Toggle Nav
-        nav.classList.toggle('nav-active')
+//     burger.addEventListener('click', ()=> {
+//         //Toggle Nav
+//         nav.classList.toggle('nav-active')
 
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        //burger animation
-        burger.classList.toggle('toggle');
+//         // Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = ''
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+//             }
+//         });
+//         //burger animation
+//         burger.classList.toggle('toggle');
+//     });
+// }
+
+// navSlide();
+
+// Menu toggle-button
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function () {
+        $("nav ul").toggleClass("showing");
     });
-}
+});
 
-navSlide();
+//Scrolling effect
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+    } else {
+        $('nav').removeClass('black');
+    }
+});
